208fad2d380e48ec6486c38d92e017b2f877d470
Mohamed Tarek <mohamedsama1212@gmail.com>
2025-10-30 20:55:19 -0400

    Finished the first part of: * Adding the Walnut App Template * Adding a Render Button

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "PathTracer"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "PathTracer"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "PathTracer"
