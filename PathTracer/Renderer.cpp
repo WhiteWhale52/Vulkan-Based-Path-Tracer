7c449b7c5cf97dfa777680d7f9da5391d8353b79
Mohamed Tarek <mohamedsama1212@gmail.com>
2025-12-31 23:55:30 -0500

    Completed implementing the lighting and shading using only diffuse lighting

--- PathTracer/Renderer.cpp
+++ PathTracer/Renderer.cpp
@@ -1,6 +1,22 @@
 #include "Renderer.h"
 
 
+namespace Utilities {
+	static uint32_t ConvertToRGBA(const glm::vec4& color) {
+		uint32_t outColor = 0x00000000;
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+		return outColor
+			| (a << 24)
+			| (r << 16)
+			| (g << 8)
+			| b;
+
+	}
+}
+
 
 void Renderer::OnResize(uint32_t width, uint32_t height) {
 
@@ -29,7 +45,10 @@ void Renderer::Render() {
 			coord = 2.0f * coord - 1.0f;
 			float aspect = (float)m_FinalImage->GetWidth() / (float)m_FinalImage->GetHeight();
 			coord.x *= aspect;
-			m_ImageData[x + m_FinalImage->GetWidth() * y] = PerPixel(coord);
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+
+			m_ImageData[x + m_FinalImage->GetWidth() * y] = Utilities::ConvertToRGBA(color);
 		}
 	}
 
@@ -39,12 +58,15 @@ void Renderer::Render() {
 
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
-	glm::vec3 rayOrigin(0.0f, 0.0f, 3.0f);
+	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
 	glm::vec3 rayDir(coord.x, coord.y, -1.0f);
 
-	glm::vec3 light(2.0f, 2.0f, 0.0f);
+	//glm::vec3 sphereColor(1.0f, 0.0f, 1.0f);
+
+	glm::vec3 lightDir(-2.0f, -1.0f, -2.0f);
+	lightDir = glm::normalize(lightDir);
 
 	float radius = 0.5f;
 
@@ -60,9 +82,24 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	float discriminant = b * b - 4.0f * a * c;
 
 	if (discriminant >= 0.0f) {
-		return 0xff00ff00;
+		/*{
+			float t_0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+			glm::vec3 hitPos = t_0 * rayDir + rayOrigin;
+			glm::vec3 normal = hitPos - glm::vec3(0.0f);
 
+			return glm::vec4(sphereColor, 1) * glm::dot(-lightOrigin, normal);
+		}*/
+			
+		{
+			float t_0 = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+			glm::vec3 hitPos = t_0 * rayDir + rayOrigin;
+			glm::vec3 normal = hitPos - glm::vec3(0.0f);
+
+			float d = glm::max(glm::dot(-lightDir, glm::normalize(normal)), 0.0f);
+
+			return glm::vec4(sphereColor, 1) * d;
+		}
 	}
 
-	return 0xff000000;
+	return glm::vec4(0,0,0,1);
 }
